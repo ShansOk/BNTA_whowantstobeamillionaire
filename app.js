f021ad74381622f8bee97e9e657f36590b48aa0b
dohuruogu123 <47153027+dohuruogu123@users.noreply.github.com>
2021-03-05 15:20:01 +0000

    finished core func + questions

--- app.js
+++ app.js
@@ -8,39 +8,39 @@ const user = new gameMaster( "Jeremy Clarkson");
 let userScore = 0;
 const winScore = 15;
 
-console.log(user);
+// console.log(user);
 console.log(user.getDetails());
 startGame();
 
 function startGame() {
-    var name = readline_sync.question('What is your name? ');
-    if (readline_sync.keyInYN(`OK ${name}, are you ready to become a millionaire? Hit Y/N`)){
-      console.log("Let's begin!");
-      getQuestion();
-    } else {
-      console.log("Alright then, have a good day!");
-    }
+  var name = readline_sync.question('What is your name? ');
+  if (readline_sync.keyInYN(`OK ${name}, are you ready to become a millionaire? Hit Y/N`)){
+    console.log("Let's begin!");
+    getQuestion();
+  } else {
+    console.log("Alright then, have a good day!");
+  }
 }
 
 function getQuestion() {
-    var arrlength = questions.games[0].questions.length;
-    var randomNumber = Math.floor(Math.random() * arrlength);
-    var randomQuestion = questions.games[0].questions[randomNumber];
+  var arrlength = questions.games[0].questions.length;
+  var randomNumber = Math.floor(Math.random() * arrlength);
+  var randomQuestion = questions.games[0].questions[randomNumber];
 
-    let userInput = readline_sync.keyInSelect(randomQuestion.content, randomQuestion.question);
-    
-    console.log(userInput);
-    checkAnswer(userInput, randomQuestion.correct)
+  let userInput = readline_sync.keyInSelect(randomQuestion.content, randomQuestion.question);
+  
+  console.log(userInput);
+  checkAnswer(userInput, randomQuestion.correct)
 }
 
 function checkAnswer(userInput, correctAnswer) {
-    if( userInput === correctAnswer) {
-        console.log('Correct');
-        userScore++;
-        checkForWin()
-    } else {
-        endGame();
-    };
+  if( userInput === correctAnswer) {
+      console.log('Correct');
+      userScore++;
+      checkForWin()
+  } else {
+      endGame();
+  };
 }
 
 function checkForWin() {
@@ -53,7 +53,7 @@ function checkForWin() {
 
 function endGame() {
   console.log("Game finished");
-  if (userScore==winScore) {
+  if (userScore == winScore) {
     console.log(`Your score is ${userScore}
     You are now a Millionaire!!!`)
   } else {
